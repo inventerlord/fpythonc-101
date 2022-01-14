8bccecc432241d373001c92b5db41cd4c4dfdc6d
inventerlord <inventerlord@gmail.com>
2022-01-14 17:24:46 +0330

    lec001

---
+++ Lec001/main.py
@@ -0,0 +1,19 @@
+# how to print
+
+# print("My name is Fatima")
+
+# data Types
+# 1. numbers
+# 2. Strings
+
+# a = 10
+# b = 3
+# c = a+b
+myname = "Fatima Jafari"
+age = 13
+myinp = input("Enter Your name = ")
+# print(c)
+# print(f"Temp is {c}° ")
+
+print(f"My name is {myinp} and i am {age} Years Old")
+
