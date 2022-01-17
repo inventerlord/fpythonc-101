7248828d115f63695b9857b3c1a35ad0f9d403da
inventerlord <inventerlord@gmail.com>
2022-01-17 18:02:41 +0330

    lec2

---
+++ Lec001/rep.py
@@ -0,0 +1,8 @@
+# how to print
+print("Some Data")
+
+# How to get Input
+
+name = input("Enter your name = ")
+
+print(name)
---
+++ Lec002/main.py
@@ -0,0 +1,10 @@
+
+# print(10+7+6+9)
+# Calculator App
+# int(string)
+val1 = int(input("value 1 = "))
+val2 = int(input("value 2 = "))
+print(val1+val2)
+
+if (val1 > 10):
+    print("good")
