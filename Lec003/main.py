002d2867da6ae4a387293af57c894c047f9c59c1
inventerlord <inventerlord@gmail.com>
2022-01-18 18:58:18 +0330

    lec003

---
+++ Lec003/main.py
@@ -0,0 +1,41 @@
+# val1 = int(input("value 1 = "))
+
+# if else 
+
+# if (val1 > 10):
+#     print("good")
+# else:
+#     print("Bad")
+
+# if elif else
+
+# if (val1 > 10 and val1 < 20):
+#     print("good")
+# elif (val1 > 20):
+#     print("Bad")
+# else:
+#     print('nothing')
+
+# loops
+
+# 1. while
+# i = 1
+# while(val1 > 0):
+#     print(f"{i} ali")
+#     val1 = val1 - 1
+#     i = i+1
+
+
+# Collection data type
+
+# 1. list
+# 2. tuple
+# 3. dict
+
+fatima = ['Fatima Jafri', 13, '7th', '123456789'] 
+fatima1 = ('Fatima Jafri', 13, '7th', '123456789')  # tuple
+fatima[3] = 987654321
+fatima1[3] = 123456789
+# print(fatima[3])
+
+print(f"Name : {fatima[0]}\nAge : {fatima[1]}\nClass : {fatima[2]}\nSSN : {fatima[3]}")
